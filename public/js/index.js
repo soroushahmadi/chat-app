c843c70790ba4c6009a4d5511beb95cf0f619e06
soroush <ahmadisorosh1@gmail.com>
2019-07-18 18:55:07 +0430

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,25 +3,12 @@ let socket = io();
 socket.on('connect', function () {
     console.log('connected to server');
 
-    socket.emit('createEmail', {
-        to: 'example@gmail.com',
-        text: 'new email from node chat app'
-    });
-
-    socket.emit('createMessage', {
-        from: 'testexample@gmail.com',
-        text: 'a new message from client one'
-    })
-
 });
 
 socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-    console.log('new email', email);
-});
 
 socket.on('newMessage', function (message) {
     console.log('new message', message);
